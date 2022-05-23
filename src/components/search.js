b2f5702fe618131e54f5238b5d179bf722d67aca
Fernando <ramirez.fernando2003@gmail.com>
2022-05-22 21:25:12 -0500

    Work on data display

--- src/components/search.js
+++ src/components/search.js
@@ -3,11 +3,17 @@ import { colors } from './colors';
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from "@mui/material/TextField";
+import { Avatar } from '@mui/material';
+import { Grid } from '@mui/material';
+
+
 import { useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { search } from '../reducers/validUser'
 import { Divider, Footer } from '.';
 import axios from 'axios';
+import { spacing } from '@mui/system';
+
 function Search() {
   // Get input from search
   const [username, setUsername] = useState("");
@@ -48,7 +54,7 @@ function Search() {
   };
 
   return (
-    <div>
+    <div style={searchContainer}>
       <form>
         <TextField 
           hiddenLabel
@@ -69,13 +75,60 @@ function Search() {
 
     <div style={dataContainer}>
         <div>
-            { userData ? <p>{userData.bio}</p> : null }
+            <Grid columns={2}>
+
+            </Grid>
+            { userData ? <Avatar alt="pfp" src={userData.avatar_url}   sx={{ width: 200, height: 200 }} /> : null}
+            <Grid container spacing={0} item sm={12} md={12}>
+
+                <Grid sm={6} md={6}>
+                { userData ? <div>{userData.name}</div> : null }
+                </Grid>
+                
+                <Grid sm={6} md={6} style={{display:'flex'}}>
+
+                    <Grid container spacing={0} item sm={12} md={12}>
+                      <Grid sm={12} md={12}>
+                        { userData ? <div>{userData.login}</div> : null }
+                      </Grid>
+
+                      <Grid>
+                        <div style={{marginRight:40}}>
+                        { userData ? <div>{userData.public_repos} repos</div> : null }
+                        </div>
+                        <div style={{marginRight:40}}>
+                        { userData ? <div>{userData.followers} followers</div> : null }
+                        </div>
+                        <div style={{marginRight:40}}>
+                        { userData ? <div>{userData.following} following</div> : null }
+                        </div>
+                      </Grid>
+
+                    </Grid>
+
+                </Grid>
+
+              </Grid>
         </div>
+
+
+
+
+
+
+
+
+
+
+
     </div>
 
   </div>
   )
 }
+const searchContainer = {
+  marginBottom: 20,
+};
 const searchStyles = {
   color: colors.white,
   fontFamily: 'Arial',
@@ -89,8 +142,7 @@ const dataContainer = {
   color: colors.white,
   fontFamily: 'Arial',
   fontSize: 40,
-  marginTop: 30,
-  borderColor: colors.white,
+  margin: 20,
   backgroundColor: colors.light_grey,
 };
 export default Search;
